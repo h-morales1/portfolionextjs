d1a318ba8b9661d5e1f5754cab6cfc06a34cd0a5
Herbert M. <hmorales5@tutanota.com>
2024-03-23 15:26:31 -0500

    fixed usage of class and Link import

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { Link } from "next";
-
+import Link from "next/link";
 export default function Document() {
   return (
     <Html className="bg-white" lang="en">
-      <header class="fixed z-10 top-0 inset-x-0 border-b bg-white">
+      <header className="fixed z-10 top-0 inset-x-0 border-b bg-white">
         <nav
-          class="flex items-center justify-between max-w-5xl
+          className="flex items-center justify-between max-w-5xl
     mx-auto px-4 h-24"
         >
           <Link
@@ -16,9 +15,9 @@ export default function Document() {
             Home
           </Link>
 
-          <div class="flex space-x-1 items-center">
+          <div className="flex space-x-1 items-center">
             <a
-              class="p-2 rounded-lg text-gray-700 hover:bg-blue-100"
+              className="p-2 rounded-lg text-gray-700 hover:bg-blue-100"
               href="https://github.com/h-morales1"
               target="_blank"
             >
