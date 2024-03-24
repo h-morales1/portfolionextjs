329949eae78136bf31fa181541f8e9a3e7e11bd4
Herbert M. <hmorales5@tutanota.com>
2024-03-23 22:15:42 -0500

    project tile now includes proj summary + proper styles

--- components/ProjectTile.js
+++ components/ProjectTile.js
@@ -1,11 +1,16 @@
-export default function ProjectTile({ id, title }) {
+import Link from "next/link";
+export default function ProjectTile({ id, title, summary }) {
   return (
     <div className="box-border h-fit w-64 p-4 border-2 border-black rounded transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
-      <h2 className="text-center">{title}</h2>
-      <p>
-        This is just a quick summary of an android project, which i worked on
-        for almost a year
-      </p>
+      <div className="text-center">
+        <Link
+          className="text-2xl font-semibold text-gray-700 my-5"
+          href={`/posts/${id}`}
+        >
+          {title}
+        </Link>
+      </div>
+      <p className="my-5 text-center">{summary}</p>
     </div>
   );
 }
