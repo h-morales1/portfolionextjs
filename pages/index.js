aa1928133d0e5e73a4d2db60ddd79b6a924d3f68
Herbert M. <hmorales5@tutanota.com>
2025-03-02 19:13:26 -0600

    add responsiveness

--- pages/index.js
+++ pages/index.js
@@ -18,22 +18,29 @@ export async function getStaticProps() {
 export default function Home({ allTilesData }) {
   return (
     <main className={`${inter.className}`}>
-      <div className="flex items-center justify-center h-fit my-32 ">
-        <Image className="" src="/main icon.svg" width={200} height={200} />
-        <h1 className="ml-24">Herbert Morales</h1>
+      <div className="flex flex-col items-center justify-center md:flex-row h-fit my-32 ">
+        <Image
+          className="h-max w-max"
+          src="/main icon.svg"
+          width={200}
+          height={200}
+        />
+        <h1 className="md:ml-24">Herbert Morales</h1>
       </div>
-      <div>
-        <h1 className="ml-24">Projects</h1>
-      </div>
-      <div className="flex flex-row gap-10 h-fit ml-24">
-        {allTilesData.map(({ id, title, summary }) => (
-          <ProjectTile
-            key={id}
-            id={id}
-            title={title}
-            summary={summary}
-          ></ProjectTile>
-        ))}
+      <div className="flex flex-col items-center">
+        <div>
+          <h1 className="md:ml-24">Projects</h1>
+        </div>
+        <div className="grid md:grid-cols-3 gap-10 h-fit md:ml-24">
+          {allTilesData.map(({ id, title, summary }) => (
+            <ProjectTile
+              key={id}
+              id={id}
+              title={title}
+              summary={summary}
+            ></ProjectTile>
+          ))}
+        </div>
       </div>
     </main>
   );
