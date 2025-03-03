f7b305ecae39094857c239f6cd2d038cf18ad50f
Herbert M. <hmorales5@tutanota.com>
2025-03-02 20:32:07 -0600

    specify height for tiles

--- components/ProjectTile.js
+++ components/ProjectTile.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 export default function ProjectTile({ id, title, summary }) {
   return (
-    <div className="box-border h-fit w-64 p-4 border-2 border-black rounded transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+    <div className="box-border h-60 w-64 p-4 border-2 border-black rounded transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
       <div className="text-center">
         <Link
           className="text-2xl font-semibold text-gray-700 my-5"
